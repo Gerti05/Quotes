d138a62535587e2d6e0f2a22beaf16c79ea11a9c
Gerti Goga <GertiGoga@gmail.com>
2019-09-19 11:17:46 -0700

    Added quote api

---
+++ src/api/index.js
@@ -0,0 +1,54 @@
+const QUOTES = [
+  {
+    Id: 1,
+    Quote: "How many cares one loses when one decides not to be something but to be someone.",
+    Person: "Gabrielle “Coco” Chanel"
+  },
+  {
+    Id: 2,
+    Quote: "Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.",
+    Person: "Dr. Seuss"
+  },
+  {
+    Id: 3,
+    Quote: "Imitation is suicide.",
+    Person: "Ralph Waldo Emerson"
+  },
+  {
+    Id: 4,
+    Quote: "Do your own thing on your own terms and get what you came here for.",
+    Person: "Oliver James"
+  },
+  {
+    Id: 5,
+    Quote: "Flatter yourself critically.",
+    Person: "Willis Goth Regier"
+  },
+  {
+    Id: 6,
+    Quote: "Do what you feel in your heart to be right, for you’ll be criticized anyway.",
+    Person: "Eleanor Roosevelt"
+  },
+  {
+    Id: 7,
+    Quote: "Whenever you find yourself on the side of the majority, it is time to pause and reflect.",
+    Person: "Mark Twain"
+  },
+  {
+    Id: 8,
+    Quote: "I will not let anyone walk through my mind with their dirty feet.",
+    Person: "Mahatma Gandhi"
+  },
+  {
+    Id: 9,
+    Quote: "Better to write for yourself and have no public, than to write for the public and have no self.",
+    Person: "Cyril Connolly"
+  },
+  {
+    Id: 10,
+    Quote: "We must not allow other people’s limited perceptions to define us.",
+    Person: "Virginia Satir"
+  },
+];
+
+export default QUOTES;
